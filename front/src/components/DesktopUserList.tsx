a8c5d9cd6d821734598b55b94fe9a323af4cb0c9
Tomas <tomasvareika85@gmail.com>
2022-03-25 15:04:44 +0200

    style desktop user list

--- front/src/components/DesktopUserList.tsx
+++ front/src/components/DesktopUserList.tsx
@@ -1,40 +1,23 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Box, Typography } from '@mui/material';
 import { User } from '../types';
+import styles from '../styles/DesktopUserList.module.scss';
 
 interface Props {
   users: User[];
 }
 
 const DesktopUserList: React.FC<Props> = ({ users }) => (
-  <Box
-    sx={{
-      py: 0.5,
-      height: 'calc(100vh - 51px)',
-      overflowY: 'scroll',
-      '&::-webkit-scrollbar': {
-        width: '0.4em'
-      },
-      '&::-webkit-scrollbar-thumb': {
-        backgroundColor: 'grey.700'
-      }
-    }}
-  >
+  <div className={styles.container}>
     {users.map((x) => (
-      <Box
-        key={x.userId}
-        sx={{ display: 'flex', alignItems: 'center', py: 0.5, cursor: 'default', '&:hover': { backgroundColor: 'grey.800' } }}
-      >
-        <AccountCircleIcon fontSize="small" sx={{ mr: 1, ml: 2 }} />
-        <Typography>{x.name}</Typography>
-      </Box>
+      <div key={x.userId} className={styles.usersContainer}>
+        <p>{x.name}</p>
+      </div>
     ))}
     {!users.length && (
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', p: 1 }}>
-        <Typography variant="subtitle2">No users connected</Typography>
-      </Box>
+      <div className={styles.emptyContainer}>
+        <p>No users connected</p>
+      </div>
     )}
-  </Box>
+  </div>
 );
 
 export default DesktopUserList;
