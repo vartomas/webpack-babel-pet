3411454319b0eebc00910936fdbcbf5d58181452
Tomas <tomasvareika85@gmail.com>
2022-03-25 10:29:23 +0200

    add css loader, sass loader and set up webpack

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
