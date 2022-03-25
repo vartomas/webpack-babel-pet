54fb8d5b08f4856869a55a3596300d05b1c249c1
Tomas <tomasvareika85@gmail.com>
2022-03-25 15:30:11 +0200

    custom styles for mobile user list

--- front/src/components/MobileUserList.tsx
+++ front/src/components/MobileUserList.tsx
@@ -1,7 +1,6 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import CloseIcon from '@mui/icons-material/Close';
-import { Box, Divider, IconButton, Paper, Typography } from '@mui/material';
+import { AiOutlineClose } from 'react-icons/ai';
 import { User } from '../types';
+import styles from '../styles/MobileUserList.module.scss';
 
 interface Props {
   users: User[];
@@ -10,49 +9,31 @@ interface Props {
 }
 
 const MobileUserList: React.FC<Props> = ({ users, open, onClose }) => (
-  <Paper
-    sx={{
-      position: 'absolute',
-      width: '100vw',
-      height: '100vh',
-      top: open ? 0 : '-100vh',
-      left: 0,
-      zIndex: 999,
-      transition: 'top .2s ease-in-out',
-      overflowY: 'scroll',
-      '&::-webkit-scrollbar': {
-        width: '0.4em'
-      },
-      '&::-webkit-scrollbar-thumb': {
-        backgroundColor: 'grey.700'
-      }
-    }}
-  >
-    <Paper sx={{ position: 'fixed', display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: 50, px: 1, width: 1 }}>
-      <Typography>Connected users</Typography>
-      <IconButton onClick={onClose}>
-        <CloseIcon />
-      </IconButton>
-    </Paper>
+  <div className={styles.container} style={{ top: open ? 0 : '-100vh' }}>
+    <div className={styles.headerContainer}>
+      <p>Connected users</p>
+      <div className={styles.iconButtonContainer} onClick={onClose}>
+        <AiOutlineClose className={styles.icon} />
+      </div>
+    </div>
 
-    <Box sx={{ pt: '50px' }}>
+    <div className={styles.userList}>
       {users.map((x) => (
-        <Box key={x.userId}>
-          <Box sx={{ display: 'flex', alignItems: 'center', py: 0.5, cursor: 'default', '&:hover': { backgroundColor: 'grey.800' } }}>
-            <AccountCircleIcon fontSize="small" sx={{ mr: 1, ml: 2 }} />
-            <Typography>{x.name}</Typography>
-          </Box>
-          <Divider />
-        </Box>
+        <div key={x.userId}>
+          <div className={styles.user}>
+            <p>{x.name}</p>
+          </div>
+          <div className={styles.divider} />
+        </div>
       ))}
 
       {!users.length && (
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', p: 1 }}>
-          <Typography variant="subtitle2">No users connected</Typography>
-        </Box>
+        <div className={styles.emptyUserList}>
+          <p>No users connected</p>
+        </div>
       )}
-    </Box>
-  </Paper>
+    </div>
+  </div>
 );
 
 export default MobileUserList;
