b740b6723e47fd2959201ed8a02a878843efa410
Tomas <tomasvareika85@gmail.com>
2022-03-30 16:42:11 +0300

    chat messages custom styles

--- front/src/components/ChatMessages.tsx
+++ front/src/components/ChatMessages.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Box, Paper, Typography } from '@mui/material';
 import { format } from 'date-fns';
 import { Message } from '../types';
+import styles from '../styles/ChatMessages.module.scss';
 
 interface Props {
   messages: Message[];
@@ -13,48 +13,21 @@ const ChatMessages: React.FC<Props> = ({ messages, messagesBottomRef, loadMoreMe
   const userId = localStorage.getItem('userId');
 
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        display: 'flex',
-        flexDirection: 'column-reverse',
-        maxHeight: [12 / 13 - 50, 12 / 13 - 50, 12 / 13],
-        overflowY: 'scroll',
-        '&::-webkit-scrollbar': {
-          width: '0.4em'
-        },
-        '&::-webkit-scrollbar-thumb': {
-          backgroundColor: 'grey.700'
-        }
-      }}
-    >
-      <Box ref={messagesBottomRef} />
+    <div className={styles.container}>
+      <div ref={messagesBottomRef} />
       {messages.map((x) => (
-        <Paper key={x._id} sx={{ p: 2, mx: 1, my: 0.5, maxWidth: 2 / 3, alignSelf: userId === x.userId ? 'end' : 'start' }}>
-          <Box sx={{ display: 'flex', alignItems: 'baseline', mb: 1 }}>
-            <Typography variant="subtitle1">{x.username}</Typography>
-            <Typography variant="subtitle2" sx={{ ml: 1, fontSize: 10 }}>
-              {format(new Date(x.date), 'dd-MM-yyyy')}
-            </Typography>
-          </Box>
-          <Typography sx={{ wordBreak: 'break-word' }}>{x.body}</Typography>
-        </Paper>
+        <div key={x._id} className={styles.message} style={{ alignSelf: userId === x.userId ? 'end' : 'start' }}>
+          <div className={styles.messageTitleContainer}>
+            <p>{x.username}</p>
+            <p className={styles.messageDate}>{format(new Date(x.date), 'dd-MM-yyyy')}</p>
+          </div>
+          <p className={styles.messageBody}>{x.body}</p>
+        </div>
       ))}
-      <Typography
-        variant="caption"
-        sx={{
-          m: 2,
-          cursor: 'pointer',
-          alignSelf: 'end',
-          '&:hover': {
-            textDecoration: 'underline'
-          }
-        }}
-        onClick={loadMoreMessages}
-      >
+      <p className={styles.loadMoreText} onClick={loadMoreMessages}>
         Load older messages
-      </Typography>
-    </Box>
+      </p>
+    </div>
   );
 };
 
